83ed3d62f3a53bce802fdfdf9828c98fafa7ce90
Malcolm Davis Steele <me@malcolmdavis.xyz>
2021-06-21 01:29:54 +0000

    Fix build

--- docs/build-doc.sh
+++ docs/build-doc.sh
@@ -4,9 +4,9 @@ if [ -d "docs" ]; then
     cd "docs" || exit
 fi
 
+mkdir -p source/tmp
 sphinx-apidoc -f -o source/ ../src/firmware
 find ../ -path '../vendor' -prune  -o -path "../docs" -prune -o \( -name '*.svg' -o -name '*.png' -o -name '*.md' \)   -exec cp --parents \{\} source/tmp \;
 make html
-pwd
-ls -Rl
+
 find source \( -name '*.svg' -o -name '*.png' -o -name '*.md' \) -exec rm {} \;
